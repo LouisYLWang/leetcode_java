70c714b8a102d2c1604f7e6f46d7e26d85c619cf
louis.yl.wang <louis.yl.wang@outlook.com>
2019-10-04 16:00:28 -0700

    update 1079 Letter Tile Possibilities

---
+++ 1079.letter-tile-possibilities.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1079 lang=java
+ *
+ * [1079] Letter Tile Possibilities
+ */
+
+// @lc code=start
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] tilesCounter = new int[26];
+        for(int i = 0; i < tiles.length(); i++){
+            int id = tiles.charAt(i) - 'A';
+            tilesCounter[id] ++;
+        }
+        return countPossibilities(tilesCounter);
+    }
+    public int countPossibilities(int[] tilesCounter){
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(tilesCounter[i] >0){
+                tilesCounter[i] --;
+                count += 1 + countPossibilities(tilesCounter);
+                tilesCounter[i] ++;
+            }
+        }
+    return count;
+    }
+}
+// @lc code=end
+
