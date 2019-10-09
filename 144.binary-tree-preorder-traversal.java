f40b5f6bc978006856cf587e840b28c8f43ed469
louis.yl.wang <louis.yl.wang@outlook.com>
2019-10-08 23:40:47 -0700

    update 144 Binary Tree Preorder Traversal

---
+++ 144.binary-tree-preorder-traversal.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // interation
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root != null){
+        // not Linkedlist but LinkedList
+        Stack<TreeNode> s = new Stack<>(); 
+        s.add(root);
+        while(s.size() != 0){
+            TreeNode cur = s.pop();
+            res.add(cur.val);
+            if(cur.right != null) s.push(cur.right);
+            if(cur.left != null) s.push(cur.left);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
